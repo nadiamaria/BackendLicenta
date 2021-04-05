4316096c024f4a4ccc28cb3bd9cbd55225603651
nadia <nadia.margescu@bearingpoint.com>
2021-04-05 21:47:25 +0300

    idk

--- src/Recipes/services/recipe.service.ts
+++ src/Recipes/services/recipe.service.ts
@@ -26,45 +26,54 @@ export class RecipeService extends TypeOrmCrudService<RecipeEntity> {
     return x;
   }
 
-  async getRecipeByParams(ingredients?: string, category?: string): Promise<any> {
+  async getRecipeByParams(
+    ingredients?: string,
+    category?: string,
+  ): Promise<any> {
     let query = this.repo
       .createQueryBuilder('recipe')
       .innerJoin('recipe.recipeIngredient', 'recipeIngredient')
       .innerJoin('recipeIngredient.ingredient', 'ingredient');
     if (category) {
-    Logger.log(category);
-
-      query =  query.innerJoin('recipe.recipeCategory', 'recipeCategory');
-      query = query.andWhere('recipeCategory.category_name = :category_name', {category_name : category});
+      Logger.log(category);
+      query = query.innerJoin('recipe.recipeCategory', 'recipeCategory');
+      query = query.andWhere('recipeCategory.category_name = :category_name', {
+        category_name: category,
+      });
     }
     if (ingredients) {
       const filter = ingredients.split(',');
-      query = query.andWhere(qb => {
-        const subQuery = qb.subQuery()
-        .select('recipe_2.id')
-        .from('recipe', 'recipe_2')
-        .innerJoin('recipe_2.recipeIngredient', 'recipeIngredient_2')
-        .innerJoin('recipeIngredient_2.ingredient', 'ingredient_2')
-        .where('recipe_2.id = recipe.id AND ingredient_2.name IN (:...ingredients)', { 
-          ingredients: filter,
-        })
-        .groupBy('recipe_2.id')
-        .andHaving('COUNT(1) = :filter_length', { filter_length : filter.length })
-        .getQuery();
+      query = query.andWhere((qb) => {
+        const subQuery = qb
+          .subQuery()
+          .select('recipe_2.id')
+          .from('recipe', 'recipe_2')
+          .innerJoin('recipe_2.recipeIngredient', 'recipeIngredient_2')
+          .innerJoin('recipeIngredient_2.ingredient', 'ingredient_2')
+          .where(
+            'recipe_2.id = recipe.id AND ingredient_2.name IN (:...ingredients)',
+            {
+              ingredients: filter,
+          })
+          .groupBy('recipe_2.id')
+          .andHaving('COUNT(1) = :filter_length', {
+            filter_length: filter.length,
+          })
+          .getQuery();
         return 'recipe.id IN ' + subQuery;
       });
-      // query = query.where('ingredient.name IN (:...ingredients)', { 
+      // query = query.where('ingredient.name IN (:...ingredients)', {
       //   ingredients: filter,
       // });
       // // query = query.addSelect('COUNT(1) AS IngredientsNumber');
       // query = query.groupBy('recipe.id')
       // .andHaving('COUNT(1) = :filter_length', { filter_length : filter.length });
       // // .where('IngredientsNumber > :filter_length', { filter_length : filter.length });
-      const result = await query.select(['recipe','ingredient']).getRawMany();
+      const result = await query.select(['recipe', 'ingredient']).getRawMany();
       return _(result)
-      .groupBy('recipe_id')
-      .map((value, key) => new Recipe_ingredients(key, value))
-      .value();
+        .groupBy('recipe_id')
+        .map((value, key) => new Recipe_ingredients(key, value))
+        .value();
     }
 
     return query.select(['recipe']).getMany();

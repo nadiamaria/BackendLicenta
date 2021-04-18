271fcf4ed85c89e537c6171668bb63a1a15d31fd
Florin <raduflorin95@gmail.com>
2021-04-18 23:50:13 +0300

    Fix user service dependendy injection issue

--- src/Users/user.module.ts
+++ src/Users/user.module.ts
@@ -3,11 +3,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { UserService } from './services/user.service';
-import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
-  controllers: [UserController],
   providers: [UserService],
+  exports: [UserService]
 })
-export class UserModule {}
+export class UserModule { }
